9c72c176f0107605017f53b1ec731d06625823c2
felix chen <felixchen3674@gmail.com>
2026-05-12 10:43:31 -0400

    react

---
+++ react-foundation/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a test</p>
+      <p>Another test</p>
+    </>
+  );
+}
+
+export default App;
