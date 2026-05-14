321b0ee9ccc4292d8496b41b5f0e19ba9de477c0
felix chen <felixchen3674@gmail.com>
2026-05-14 16:01:59 -0400

    input

--- react-foundation/src/App.tsx
+++ react-foundation/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Day12 from "./day12-components/Day12";
 import Day13 from "./day13-states/Day13";
+import Day14 from "./day14-inputs/Day14";
 
 export default function App() {
   return (
     <div>
       {/* <Day12 /> */}
-      <Day13 />
+      {/* <Day13 /> */}
+      <Day14 />
     </div>
   );
 }
---
+++ react-foundation/src/day14-inputs/Day14.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import EventHandlers from "./EventHandlers";
+import Input from "./Input";
+import SignUpForm from "./SignUpForm";
+import Select from "./Select";
+import CombingInputStates from "./CombingInputStates";
+
+export default function Day14() {
+  return (
+    <div>
+      <h1>day 14</h1>
+      {/* <EventHandlers /> */}
+      {/* <Input /> */}
+      {/* <Select /> */}
+      {/* <SignUpForm /> */}
+      <CombingInputStates />
+    </div>
+  );
+}
